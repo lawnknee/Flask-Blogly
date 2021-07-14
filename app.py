16666dc8a25e5353b1b912d83ccdd429bef66d47
Loni <kuangloni@gmail.com>
2021-07-14 13:11:12 -0700

    routes done

--- app.py
+++ app.py
@@ -18,9 +18,51 @@ debug = DebugToolbarExtension(app)
 @app.route('/')
 def main_page():
 
-	return render_template('user_listing.html', user="Someone")  # TODO: rename user for later
+    return redirect('/users')
+                 
+@app.route('/users')
+def show_users():
+    
+    return render_template('user_listing.html', user="Someone")  # TODO: rename user for later
 
-@app.route('/users/new')
+@app.route('/users/new', methods=["GET"])
 def show_add_user_form():
 
-	return render_template('user_form.html')
+    return render_template('user_form.html')
+
+@app.route('/users/new', methods=["POST"])
+def add_user():
+    first_name = request.form['first']
+    last_name = request.form['last']
+    image_url = request.form['img-url']
+    
+    # insert user into user database
+    # pull back users and pass into redirect
+    
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>')
+def show_user_info(user_id):
+    
+    return render_template('user_detail.html')
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user_info(user_id):
+    
+    return render_template('user_edit.html')
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def process_user_edit(user_id):
+
+    # process edit form
+    # update database
+    # get updated user info back from database
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def delete_user(user_id):
+    
+    # delete user from database
+    
+    return redirect('/')
---
+++ test_app.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from app import app, models
+
+# Make Flask errors be real errors, not HTML pages with error info
+app.config['TESTING'] = True
+
+# This is a bit of hack, but don't use Flask DebugToolbar
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
