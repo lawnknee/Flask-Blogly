a6ca0a2b91b4081ee09a549e7b173786b8da17d2
Loni <kuangloni@gmail.com>
2021-07-14 18:47:14 -0700

    brits code review

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-default_url = 'https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg'
+DEFAULT_URL = 'https://rithm-students-media.s3.amazonaws.com/CACHE/images/user_photos/brit/480dfb17-4431-4d8d-86f9-6c85b6571e7b-brit/819782924da7552d2e53d3d14da53865.jpeg'
 
 def connect_db(app):
     """Connect to database."""
@@ -24,16 +24,20 @@ class User(db.Model):
                            nullable=False)
     last_name = db.Column(db.Text,
                           nullable=False)
-    image_url = db.Column(db.Text)
+    image_url = db.Column(db.Text,
+                          default=DEFAULT_URL)
     
     
+    @classmethod
+    def add_new_user(cls, first, last, image):
+        # if image == "":
+        #     image = default_url
+        
+        # image = image if (image) else DEFAULT_URL
+        
 
-def add_new_user(first, 
-                 last, 
-                 image = default_url):
-
-    image=image if (not image) else default_url
-
-    new_user = User(first_name=first, last_name=last, image_url=image)
-    db.session.add(new_user)
-    db.session.commit()
+        new_user = cls(first_name=first, last_name=last, image_url=image)
+        db.session.add(new_user)
+        # db.session.commit() 
+        # should never commit in method
+        # always commit in the view function to close out the transaction
