67f60a52f294429824357c37b45d7dd1592a630b
Loni <kuangloni@gmail.com>
2021-07-15 13:02:57 -0700

    direct add post cancel back to user details

--- seed.py
+++ seed.py
@@ -1,3 +1,25 @@
-# add users
+"""Seed file to make sample data for Users and Posts"""
+
+from models import Users, Posts
+from app import app
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# If table isn't empty, empty it
+Users.query.delete()
+Posts.query.delete()
+
+# Add users
 loni = User(first_name='Loni', last_name='Kuang', image_url='https://rithm-students-media.s3.amazonaws.com/CACHE/images/user_photos/loni-kuang/0bca5f22-fd55-4371-9eda-938d8d66ef53-profile/f62d8ba33b4d110ff875b3bdd6de2848.jpg')
 ray = User(first_name='Ray', last_name='Agni', image_url='https://rithm-students-media.s3.amazonaws.com/CACHE/images/user_photos/jhensen-agni/6a11f561-4a5a-4e77-a759-929a9f37f766-37388027/2cfc9e8d67110fecb58ff8d62d99b668.jpeg')
+bob = User(first_name='Bob', last_name='Smith')
+
+# Add new user objects to session, so they'll persist
+db.session.add(loni)
+db.session.add(ray)
+db.session.add(bob)
+
+# Add posts
+
